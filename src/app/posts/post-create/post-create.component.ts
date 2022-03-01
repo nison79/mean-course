f4ddb9d1adcb18c11f1862b7be5f382ea22692f7
George Nikoglou <georgenison@gmail.com>
2022-03-01 19:52:11 +0200

    material

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent implements OnInit {
+  newPost = 'No Content';
+  enteredValue = '';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
